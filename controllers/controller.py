f4200c09aa546a159a0f02491cb747889f0baf23
Paul Mascall <pjwmascall@hotmail.com>
2022-01-30 11:24:42 +0000

    added random choice maker and game logic

--- controllers/controller.py
+++ controllers/controller.py
@@ -6,3 +6,16 @@ from modules.game import Game
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/<choice1>', methods=["POST"])
+def get_choices(choice1):
+    choice2 = Player.make_choice()
+    return redirect(f'/{choice1}/{choice2}')
+
+@app.route('/<choice1>/<choice2>')
+def play_a_game(choice1, choice2):
+    player1 = Player("John", choice1)
+    player2 = Player("Computer", choice2)
+    game = Game(player1, player2)
+    winner = game.play_game()
+    return render_template('results.html', winner=winner)
--- modules/game.py
+++ modules/game.py
@@ -3,3 +3,14 @@ class Game:
     def __init__(self, player1, player2):
         self.player1 = player1
         self.player2 = player2
+
+    def play_game(self):
+        if self.player1.choice == self.player2.choice:
+            return None
+        choices = {self.player1.choice: self.player1.name, self.player2.choice: self.player2.name}
+        if "rock" in choices and "scissors" in choices:
+            return choices["rock"]
+        if "scissors" in choices and "paper" in choices:
+            return choices["scissors"]
+        if "paper" in choices and "rock" in choices:
+            return choices["paper"]
